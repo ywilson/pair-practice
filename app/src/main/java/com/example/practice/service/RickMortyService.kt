307c41bf7d3c3734da68af2ffa0967fc756005cf
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-10-08 16:15:37 -0500

    build getCharacters with tests

--- app/src/main/java/com/example/practice/service/RickMortyService.kt
+++ app/src/main/java/com/example/practice/service/RickMortyService.kt
@@ -3,10 +3,8 @@ package com.example.practice.service
 import com.apollographql.apollo.ApolloClient
 import com.example.practice.CharacterQuery
 
-import com.example.practice.type.Character
 
-
-class RickMortyService(serverUrl: String = "https://rickandmortyapi.com/graphql") {
+class RickMortyService(private val serverUrl: String = "https://rickandmortyapi.com/graphql") {
     private val apolloClient = ApolloClient.Builder().serverUrl(serverUrl).build()
 
     suspend fun getCharacters(nameSearch: String) : List<CharacterQuery.Result?> {
--- app/src/test/java/com/example/practice/service/RickMortyServiceTest.kt
+++ app/src/test/java/com/example/practice/service/RickMortyServiceTest.kt
@@ -3,11 +3,13 @@ package com.example.practice.service
 import com.apollographql.apollo.api.toJson
 import com.apollographql.mockserver.MockResponse
 import com.apollographql.mockserver.MockServer
-import com.apollographql.mockserver.enqueueString
 import com.example.practice.CharacterQuery
 import com.example.practice.type.buildCharacter
 import com.example.practice.type.buildCharacters
 import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
@@ -15,12 +17,15 @@ import org.mockito.junit.MockitoJUnitRunner
 @RunWith(MockitoJUnitRunner::class)
 class RickMortyServiceTest
 {
+    lateinit var mockServer : MockServer
+    @Before
+    fun setUp() {
+        mockServer = MockServer()
+    }
+
     @Test
     fun `when getCharacters call, then return list of Characters`() = runTest {
-        val mockServer = MockServer()
-        MockResponse.Builder()
-
-        val query = CharacterQuery.Data{
+        val expectedCharactersData = CharacterQuery.Data{
             characters = buildCharacters {
             results = listOf(
                 buildCharacter {
@@ -29,11 +34,22 @@ class RickMortyServiceTest
                 }
             )
         }}
-        mockServer.enqueueString(query.toJson())
+
+        val mockResponseBuilder = MockResponse.Builder()
+        mockResponseBuilder.body("{\"data\":${expectedCharactersData.toJson()}}")
+        mockResponseBuilder.statusCode(200)
+
+        mockServer.enqueue(mockResponseBuilder.build())
 
         val rickMortyService = RickMortyService(mockServer.url() )
 
         val actualResponse = rickMortyService.getCharacters("mom")
 
+        assertEquals(expectedCharactersData.characters?.results, actualResponse)
+    }
+
+    @After
+    fun tearDown() {
+        mockServer.close()
     }
 }
