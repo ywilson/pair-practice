cff5c67c9101c77269df2637c240321c46928ff2
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-10-09 16:01:54 -0500

    build character state flow

--- app/src/main/java/com/example/practice/characters/CharactersViewModel.kt
+++ app/src/main/java/com/example/practice/characters/CharactersViewModel.kt
@@ -1,14 +1,20 @@
 package com.example.practice.characters
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.practice.repository.CharacterRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class CharactersViewModel @Inject constructor(private val characterRepository: CharacterRepository): ViewModel() {
-
-
-    fun refreshCharacters() {
-
+    private val _characterFlow = MutableStateFlow<List<Character>>(emptyList())
+    val characterFlow = _characterFlow.asStateFlow()
+    
+    fun refreshCharacters() = viewModelScope.launch(Dispatchers.IO) {
+        _characterFlow.value = characterRepository.getCharacters("")
     }
 
 }
--- app/src/test/java/com/example/practice/characters/CharactersViewModelTest.kt
+++ app/src/test/java/com/example/practice/characters/CharactersViewModelTest.kt
@@ -1,11 +1,20 @@
 package com.example.practice.characters
 
 import com.example.practice.repository.CharacterRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 
 @RunWith(MockitoJUnitRunner::class)
 class CharactersViewModelTest {
@@ -16,10 +25,34 @@ class CharactersViewModelTest {
     @InjectMocks
     lateinit var charactersViewModel: CharactersViewModel
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `when refreshCharacters called, then pull characters from repo and inform view`()
-    {
+    fun `when refreshCharacters called, then pull characters from repo and inform view`() =
+     runTest {
+        val testCharacters = listOf(
+            Character(
+                name = "mom",
+                image = "wave.png"
+            ),
+            Character(
+                name = "test",
+                image = "wave3.png"
+            ),
+            Character(
+                name = "dad",
+                image = "clap.png"
+            )
+        )
+         whenever(mockCharactersRepository.getCharacters("")).thenReturn(testCharacters)
 
         charactersViewModel.refreshCharacters()
+
+        verify(mockCharactersRepository).getCharacters("")
+
+        val job = launch (UnconfinedTestDispatcher(testScheduler)){charactersViewModel.characterFlow.collect {
+            assertEquals(testCharacters, it)
+        }}
+
+         job.cancel()
     }
 }
