c6c32ec77402bdf9128250ef0a4d0f22c3e16593
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-10-09 16:23:49 -0500

    show character list

--- app/src/main/java/com/example/practice/MainActivity.kt
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -12,11 +12,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.practice.characters.Character
 import com.example.practice.characters.CharactersViewModel
 import com.example.practice.ui.theme.PracticeTheme
 import dagger.hilt.android.AndroidEntryPoint
-import com.example.practice.characters.Character
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
