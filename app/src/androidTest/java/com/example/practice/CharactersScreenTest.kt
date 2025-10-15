f96c4652feba3dea0f5e26d9b2f1cd07850f39db
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-10-15 12:30:50 -0500

    build unit tests for CharacterScreen

---
+++ app/src/androidTest/java/com/example/practice/CharactersScreenTest.kt
@@ -0,0 +1,49 @@
+package com.example.practice
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.example.practice.characters.Character
+import com.example.practice.characters.CharacterData
+import com.example.practice.characters.CharactersScreen
+import org.junit.Rule
+import org.junit.Test
+
+class CharactersScreenTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun whenGivenSuccessCharacterData_thenDisplayCharacterList()
+    {
+        val testCharacterData = CharacterData.Success(
+            listOf(
+                Character("mom", "wave.png"),
+                Character("dad", "high5.png")
+            ))
+
+
+        composeTestRule.setContent {
+            CharactersScreen(testCharacterData)
+        }
+
+        testCharacterData.characters.forEach {
+            composeTestRule.onNodeWithText(it.name)
+                .assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun whenGivenErrorCharacterData_thenDisplayErrorMessage()
+    {
+        val errorCharacterData = CharacterData.Error("Unknown error occurred")
+
+        composeTestRule.setContent {
+            CharactersScreen(errorCharacterData)
+        }
+
+        composeTestRule
+            .onNodeWithText("Unknown error occurred")
+            .assertIsDisplayed()
+    }
+}
