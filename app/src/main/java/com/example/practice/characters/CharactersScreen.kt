638856e16440d5e45748c6639c4fba3f08af9a6b
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-10-15 13:39:23 -0500

    center align error and loading

--- app/src/main/java/com/example/practice/characters/CharactersScreen.kt
+++ app/src/main/java/com/example/practice/characters/CharactersScreen.kt
@@ -1,12 +1,14 @@
 package com.example.practice.characters
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -16,8 +18,8 @@ fun CharactersScreen(characterData : CharacterData) {
         when (characterData)
         {
             is CharacterData.Success -> CharacterList(characterData.characters)
-            is CharacterData.Error -> Text(text = characterData.message)
-            is CharacterData.Loading -> CircularProgressIndicator()
+            is CharacterData.Error -> Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {Text(text = characterData.message)}
+            is CharacterData.Loading -> Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center){CircularProgressIndicator()}
         }
     }
 }
