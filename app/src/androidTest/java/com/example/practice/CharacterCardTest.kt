e15ab274b4ceecf244848aeda92c1de0a056f023
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-10-15 13:42:51 -0500

    setup for building the character card

---
+++ app/src/androidTest/java/com/example/practice/CharacterCardTest.kt
@@ -0,0 +1,9 @@
+package com.example.practice
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+
+class CharacterCardTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+}
---
+++ app/src/main/java/com/example/practice/characters/CharacterCard.kt
@@ -0,0 +1,5 @@
+package com.example.practice.characters
+
+class CharacterCard {
+
+}
