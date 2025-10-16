56455fa459f5d1b1138be753e39fad4e114de45d
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-10-16 12:03:23 -0500

    styling shennanigans

--- app/src/main/java/com/example/practice/characters/CharacterCard.kt
+++ app/src/main/java/com/example/practice/characters/CharacterCard.kt
@@ -1,21 +1,29 @@
 package com.example.practice.characters
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.practice.R
+import com.example.practice.characters.Character
 
 @Composable
-fun CharacterCard(character: Character) {
-    Card {
-        Column {
-            Text(text = character.name)
+fun CharacterCard(
+    character: Character) {
+    Card (modifier = Modifier.width(130.dp).fillMaxHeight()) {
+        Column (modifier = Modifier.padding(5.dp), horizontalAlignment = Alignment.CenterHorizontally){
+            Text(modifier = Modifier.widthIn(max = 120.dp).height(22.dp), text = character.name, overflow = TextOverflow.Ellipsis)
             AsyncImage(
                 placeholder = painterResource(R.drawable.baseline_broken_image_24),
                 model = ImageRequest.Builder(LocalContext.current)
@@ -27,4 +35,10 @@ fun CharacterCard(character: Character) {
             Text(text = character.status)
         }
     }
+}
+
+@Preview
+@Composable
+fun CharacterCardPreview(){
+    CharacterCard(Character("suuuupeerrrrr longggggg azzzzzzz nammmmmeeeeees", "http://dog.img", "dead"))
 }
--- app/src/main/java/com/example/practice/characters/CharactersScreen.kt
+++ app/src/main/java/com/example/practice/characters/CharactersScreen.kt
@@ -1,24 +1,22 @@
 package com.example.practice.characters
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CharactersScreen(characterData : CharacterData) {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-    Scaffold(modifier = Modifier.fillMaxSize()) {  _ ->
+    Scaffold(modifier = Modifier.fillMaxSize()) {  padding ->
         when (characterData)
         {
-            is CharacterData.Success -> CharacterList(characterData.characters)
+            is CharacterData.Success -> CharacterList(characterData.characters, padding)
             is CharacterData.Error -> Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {Text(text = characterData.message)}
             is CharacterData.Loading -> Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center){CircularProgressIndicator()}
         }
@@ -26,10 +24,14 @@ fun CharactersScreen(characterData : CharacterData) {
 }
 
 @Composable
-fun CharacterList(characters: List<Character>) {
-    LazyColumn(modifier = Modifier.fillMaxWidth()){
-        items(characters.size) {
-            CharacterCard(characters[it])
+fun CharacterList(characters: List<Character>, padding: PaddingValues) {
+   LazyColumn(modifier = Modifier.padding(padding)){
+        items(characters.chunked(3)) {rowItems->
+            Row (modifier = Modifier.fillMaxWidth().padding(bottom = 10.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+                rowItems.forEach {
+                    CharacterCard(it)
+                }
+            }
         }
     }
 }
