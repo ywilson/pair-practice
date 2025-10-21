88224e037d7ead45810357ddeb37c0299140ff0e
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-10-21 13:36:57 -0500

    add character details to character data elements and query

--- app/src/main/java/com/example/practice/characters/Character.kt
+++ app/src/main/java/com/example/practice/characters/Character.kt
@@ -1,6 +1,24 @@
 package com.example.practice.characters
 
-data class Character(val name: String = "",
-                     val image: String = "",
-                     val status : String = "",
-                     val gender : String = "")
+data class Character(
+    val name: String = "",
+    val image: String = "",
+    val status: String = "",
+    val gender: String = "",
+    val species: String = "",
+    val type : String = "",
+    val origin : Location = Location(),
+    val location: Location = Location(),
+    val episodes: List<Episode> = listOf(Episode())
+)
+
+data class Location(
+    val name : String = "",
+    val type : String = "",
+    val dimension: String = ""
+)
+
+data class Episode(
+    val name : String = "",
+    val episode: String = ""
+)
--- app/src/main/java/com/example/practice/repository/CharacterMapperExt.kt
+++ app/src/main/java/com/example/practice/repository/CharacterMapperExt.kt
@@ -2,7 +2,40 @@ package com.example.practice.repository
 
 import com.example.practice.CharacterQuery
 import com.example.practice.characters.Character
+import com.example.practice.characters.Episode
+import com.example.practice.characters.Location
 
-fun CharacterQuery.Result.toCharacter() = Character(name ?: "", image ?: "", status ?: "", gender ?: "")
+fun CharacterQuery.Result.toCharacter() = Character(
+    name ?: "",
+    image ?: "",
+    status ?: "",
+    gender ?: "",
+    species ?: "",
+    type ?: "",
+    origin?.toLocation() ?: Location(),
+    location?.toLocation() ?: Location(),
+    episode.toEpisodeList()
+)
+
+fun CharacterQuery.Origin.toLocation() = Location(
+    name ?: "",
+    type ?: "",
+    dimension ?: ""
+)
+
+fun CharacterQuery.Location.toLocation() = Location(
+    name ?: "",
+    type ?: "",
+    dimension ?: ""
+)
+
+fun List<CharacterQuery.Episode?>.toEpisodeList() : List<Episode> {
+    return mapNotNull { episode -> episode?.toEpisode() }
+}
+
+fun CharacterQuery.Episode.toEpisode() = Episode(
+    name ?: "",
+    episode ?: ""
+)
 
 
